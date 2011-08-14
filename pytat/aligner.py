25ab50a881fabce1183d4dcd687989d2ef96e138
John Tye Bennett <john.tye.bennett@gmail.com>
2011-08-14 14:54:40 -0700

    Changes align_add to handle arbitrary number of lists.

--- pytat/aligner.py
+++ pytat/aligner.py
@@ -1,19 +1,14 @@
-def align_add(key_func, xs1, xs2, placeholder_func = lambda key: None):
+def align_add(key_func, placeholder_func, *lists):
     current = 0
-    while current < len(xs1) and current < len(xs2):
-        key1 = key_func(xs1[current])
-        key2 = key_func(xs2[current])
-        if key1 < key2:
-            xs2.insert(current, placeholder_func(key1))
-        elif key1 > key2:
-            xs1.insert(current, placeholder_func(key2))
-        current += 1
-    while current < len(xs1):
-        xs2.append(placeholder_func(key_func(xs1[current])))
-        current += 1
-    while current < len(xs2):
-        xs1.append(placeholder_func(key_func(xs2[current])))
-        current += 1
+    while current < max(len(xs) for xs in lists):
+        repeat = False
+        key = min(key_func(xs[current]) for xs in lists if len(xs) > current)
+        for xs in lists:
+            if current >= len(xs) or key_func(xs[current]) > key:
+                repeat = True
+                xs.insert(current, placeholder_func(key))
+        if not repeat:
+            current += 1
 
 def align_delete(key_func, *lists):
     current = 0
--- test/aligner_test.py
+++ test/aligner_test.py
@@ -7,14 +7,20 @@ class AlignerTest(unittest.TestCase):
     def test_align_add(self):
         xs = [(1, 3), (2, 1), (4, 7), (5, 9)]
         ys = [(0, 1), (2, 8), (3, 6), (5, 5), (6, 4), (7, 1)]
+        zs = []
+        ws = [(5, 1)]
         align_add(
             lambda x: x[0],
+            lambda key: (key, 0),
             xs,
             ys,
-            lambda key: (key, 0),
+            zs,
+            ws
         )
         self.assertEqual([(0, 0), (1, 3), (2, 1), (3, 0), (4, 7), (5, 9), (6, 0), (7, 0)], xs)
         self.assertEqual([(0, 1), (1, 0), (2, 8), (3, 6), (4, 0), (5, 5), (6, 4), (7, 1)], ys)
+        self.assertEqual([(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)], zs)
+        self.assertEqual([(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 1), (6, 0), (7, 0)], ws)
 
     def test_align_delete(self):
         xs = [(1, 3), (2, 1), (4, 7), (5, 9)]
